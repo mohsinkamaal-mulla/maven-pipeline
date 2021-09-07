242c2d12b2ffe53a551bf61410025ad6a9b1b31e
mohsinkamaal <mohsinkamaal.mulla@gmail.com>
2021-09-07 09:52:50 +0000

    Updated test/mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "*****Testing the code******"
 echo "***************************"
 
-#WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/ubuntu/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
