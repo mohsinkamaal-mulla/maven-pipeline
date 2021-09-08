1039f7c07b1ac5d45aff770d200d44b360799aad
Mohsinkamaal Mulla <76730312+mohsinkamaal-mulla@users.noreply.github.com>
2021-09-08 12:30:08 +0530

    Update App.java

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from Jenkins Pipeline!";
+    private final String message = "Hello from Jenkins Pipeline-Automate!";
 
     public App() {}
 
