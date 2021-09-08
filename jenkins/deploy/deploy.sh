1e87ff9acef516df4d07072e695d12b3de4ee7b2
Mohsinkamaal Mulla <76730312+mohsinkamaal-mulla@users.noreply.github.com>
2021-09-08 13:54:27 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@15.206.93.71:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@15.206.93.71:/tmp/publish
-ssh -i /opt/prod prod-user@15.206.93.71 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth prod-user@65.2.33.153:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@65.2.33.153:/tmp/publish
+ssh -i /opt/prod prod-user@65.2.33.153 "/tmp/publish"
